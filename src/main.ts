f1a6e41ebd712344b88cd6541359674563edd64a
Prosper <prosper.gyinka@coderaccoon.com>
2026-08-03 07:25:37 +0000

    Fix port configuration

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT ?? 3030;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port);
 }
 bootstrap();
